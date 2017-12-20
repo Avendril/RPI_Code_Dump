9f665916cf13795085a15726cc24852e0b471680
Bartek Chmielewski <bchmielewski@tssg.org>
2017-12-20 16:08:10 +0000

    Infinite Errors

--- master.py
+++ master.py
@@ -9,19 +9,22 @@ hostname = "87.44.19.170"#Ubuntu VM: 87.44.19.170  Local Machine(bchmielewski la
 temperatureQueue1 = "SmartHive/Temperature/Temp1"
 temperatureQueue2 = "SmartHive/Temperature/Temp2"
 
-try:
-	print("Starting mqtt publishing")
+print("Starting mqtt publishing")
 
-	while True:
-		os.system("mosquitto_pub -h " + hostname + " -t SmartHive/Hostname -m " + hostname)
+while True:
+	try:
+
+		while True:
+			os.system("mosquitto_pub -h " + hostname + " -t SmartHive/Hostname -m " + hostname)
 	
-		os.system("mosquitto_pub -h " + hostname + " -t " + temperatureQueue1 + " -m " + temperature.Read_Temp1())
-		database.upload_to_db('temperature', temperature.Read_Temp1())
+			os.system("mosquitto_pub -h " + hostname + " -t " + temperatureQueue1 + " -m " + temperature.Read_Temp1())
+			database.upload_to_db('temperature', temperature.Read_Temp1())
 
-		os.system("mosquitto_pub -h " + hostname + " -t " + temperatureQueue2 + " -m " + temperature.Read_Temp2() )
-		database.upload_to_db('temperature2', temperature.Read_Temp2())
+			os.system("mosquitto_pub -h " + hostname + " -t " + temperatureQueue2 + " -m " + temperature.Read_Temp2() )
+			database.upload_to_db('temperature2', temperature.Read_Temp2())
 
-		time.sleep(5)
+			time.sleep(5)
 
-except Exception as e:
-	pass
+	except:
+		print("Error,AGAIN")
+		pass
--- masterGyro.py
+++ masterGyro.py
@@ -5,21 +5,23 @@ import os
 import gyroscope as gyro
 import testDB as database
 
-try:
-        print("Starting mqtt publishing")
+print("Starting mqtt publishing")
 
-        while True:
+	try:
 
-                os.system("mosquitto_pub -h 87.44.19.170 -t SmartHive/Gyroscope/Axis-X -m " + gyro.Gyroscope_Read_X())
-                database.upload_to_db('gyro_axis_x', gyro.Gyroscope_Read_X())
-                os.system("mosquitto_pub -h 87.44.19.170 -t SmartHive/Gyroscope/Axis-Y -m " + gyro.Gyroscope_Read_Y())
-                database.upload_to_db('gyro_axis_y', gyro.Gyroscope_Read_Y())
-                os.system("mosquitto_pub -h 87.44.19.170 -t SmartHive/Gyroscope/Axis-Z -m " + gyro.Gyroscope_Read_Z())
-                database.upload_to_db('gyro_axis_z', gyro.Gyroscope_Read_Z())
-                time.sleep(10)
+	        while True:
 
-except Exception:
-	pass
+        	        os.system("mosquitto_pub -h 87.44.19.170 -t SmartHive/Gyroscope/Axis-X -m " + gyro.Gyroscope_Read_X())
+             		database.upload_to_db('gyro_axis_x', gyro.Gyroscope_Read_X())
+             		os.system("mosquitto_pub -h 87.44.19.170 -t SmartHive/Gyroscope/Axis-Y -m " + gyro.Gyroscope_Read_Y())
+	                database.upload_to_db('gyro_axis_y', gyro.Gyroscope_Read_Y())
+	                os.system("mosquitto_pub -h 87.44.19.170 -t SmartHive/Gyroscope/Axis-Z -m " + gyro.Gyroscope_Read_Z())
+	                database.upload_to_db('gyro_axis_z', gyro.Gyroscope_Read_Z())
+	                time.sleep(10)
+
+	except Exception:
+		print("ERROR,AGAIN")
+		pass
 #except KeyboardInterrupt:
 #        print("Loop was stopped manually")
 
