04466acf3def647276e1c7b3718e2576dd00f7b4
Bartek Chmielewski <bchmielewski@tssg.org>
2017-12-20 09:53:41 +0000

    Rest of files

--- masterTest.py
+++ masterTest.py
@@ -5,9 +5,12 @@ import os
 import testDB as database
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
+try:
+	while True:
+	    data = ser.readline()
+	#    print data
+	    os.system("mosquitto_pub -h 87.44.19.170 -t SmartHive/Weight -m " + data)
+	    database.upload_to_db_String('weight', data)
 
-while True:
-    data = ser.readline()
-#    print data
-    os.system("mosquitto_pub -h 87.44.19.170 -t SmartHive/Weight -m " + data)
-    database.upload_to_db_String('weight', data)
+except Exception as e:
+	pass
